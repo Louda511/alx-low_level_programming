ae4cf31f9c281f549f0bc551d25200fb7991ac84
Ahmed Khaled <86980008+Louda511@users.noreply.github.com>
2023-04-13 21:58:03 +0200

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,13 +3,13 @@
 /**
  * main - prints program name, followed by a new line
  * @argc: int
- * @argv: array of strings
- * return: 0
+ * @argv: list
+ * Return: 0
  */
 
-int main(int argc, char* argv[])
+int main(int argc, char const *argv[])
 {
 	(void)argc;
 	printf("%s\n", argv[0]);
-	return(0);
+	return (0);
 }
