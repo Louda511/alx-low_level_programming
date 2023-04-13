67a0ed1d65130681a643e8ffad098717608b2a1b
Louda511 <eng.ahmed.khaled.511@gmail.com>
2023-04-13 21:16:57 +0200

    hot fix 0x01

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,7 +7,8 @@
  *Return:0-success, non-zero-fail.
  */
 
-int main (int argc, char* argv[] __attribute__((unused))){
+int main (int argc, char* argv[] __attribute__((unused)))
+{
 	printf("%d\n",argc-1);
         return(0);
 }
