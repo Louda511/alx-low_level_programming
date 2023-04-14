4f83f6209d4ea9d345ecee4e023f1f1bdeccf611
Louda511 <eng.ahmed.khaled.511@gmail.com>
2023-04-14 16:42:21 +0200

    0x0B added

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include <main.h>
+
+/**
+ * *create_array - allocates memory for an array of chars
+ * @size: size of the array
+ * @c: initial value of all array
+ *
+ * Return: pointer to the start of the array or NULL
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *array = malloc(size * sizeof(char));
+
+	if (size == 0 || array == 0)
+		return (NULL);
+
+	while (size--)
+	{
+		array[size] = c;
+	}
+
+	return (array);
+}
