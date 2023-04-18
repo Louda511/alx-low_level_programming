0d1b3f728701d995d797b2ef360387a1f1b4448e
Louda511 <eng.ahmed.khaled.511@gmail.com>
2023-04-18 20:53:45 +0200

    $msg

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_dog - frees memory dynamically allocated to dog
+ * @d - pointer to struct
+ *
+ * Return: nothing
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
+		free(d);
+	}
+}
