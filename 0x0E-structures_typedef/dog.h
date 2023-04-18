83354a9b087500e97d1f6c192ccf58adedf21210
Louda511 <eng.ahmed.khaled.511@gmail.com>
2023-04-18 19:13:14 +0200

    $msg

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - stores info about object dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: a structure to define entity of a dog
+ *
+ */
+
 struct dog
 {
 	char *name;
