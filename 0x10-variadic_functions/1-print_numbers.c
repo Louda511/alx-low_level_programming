4286048b7ffcca70ce53a1fb2895fd8e02c6b95d
Louda511 <eng.ahmed.khaled.511@gmail.com>
2023-04-20 22:43:43 +0200

    $msg

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print numbers seprated by seorator
+ * @separator: the character that seprates integers
+ * @n: number of integers
+ *
+ * Return: nothing
+ *
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	if (n != 0)
+	{
+		va_list args;
+		unsigned int i = n;
+		int num;
+
+		if (!separator)
+			separator = "";
+
+		va_start(args, n);
+		while (--i)
+		{
+			num = va_arg(args, int);
+			printf("%d%s", num, separator);
+		}
+		num = va_arg(args, int);
+		va_end(args);
+		printf("%d\n", num);
+	}
+
+}
