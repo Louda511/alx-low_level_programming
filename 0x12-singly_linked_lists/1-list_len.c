7600236d88fadc18f0f3f43ad41234ff7abb0cdf
Louda511 <eng.ahmed.khaled.511@gmail.com>
2023-04-27 20:03:36 -0700

    added 1 and hot fix 0-

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,20 +1,19 @@
 #include "lists.h"
 
 /**
- * print_list - prints all elements in a list
+ * list_len - prints all elements in a list
  * @h: pointer to the first node
  *
  * Return: number of elements in a list
  *
  */
 
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
 	size_t n = 0;
 
 	while (h != NULL)
 	{
-		h->str ? printf("[%d] %s\n", h->len, h->str) : printf("[0] (nil)\n");
 		n++;
 		h = h->next;
 	}
