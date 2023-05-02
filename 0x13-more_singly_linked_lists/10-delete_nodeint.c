b790e1076caff89277261cdd9007c6a7c8ff3a1c
Louda511 <eng.ahmed.khaled.511@gmail.com>
2023-05-01 23:01:44 -0700

    hot fix 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -19,14 +19,16 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		node = *head;
 		while (node)
 		{
-			if (i == index)
-			{
+			
+			
 				if (index == 0)
 				{
 					*head = node->next;
 					free(node);
 					return (1);
 				}
+			if (i == index-1)
+			{
 				d_node = node->next;
 				node->next = (d_node)->next;
 				free(d_node);
