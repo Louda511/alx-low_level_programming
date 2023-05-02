652bcbd4da35f9c2b210568ed60ab47278c7ffd9
Louda511 <eng.ahmed.khaled.511@gmail.com>
2023-05-01 21:24:32 -0700

    added 9-insert_nodeint.c

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - adds a node at a certain index
+ * @head: pointer to pointer to the first node
+ * @idx: index where the node will be put
+ * @n: data value of the node
+ *
+ * Return: Address of the new node or NULL if it fails
+ *
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *node, *prevnode;
+	unsigned int i;
+	prevnode = *head;
+
+	
+	if (head && *head)
+	{
+		node = malloc(sizeof(listint_t));
+		if (!node)
+			return (NULL);
+		node->n = n;
+
+		if (idx == 0)
+		{
+			node->next = (*head)->next;
+			*head = node;
+		}
+		
+		for (i = 1; i <= idx - 1; i++)
+			prevnode = prevnode->next;
+
+		node->next = prevnode->next;
+		prevnode->next = node;
+
+		return (node);
+	}
+
+	return (NULL);
+}
+
+
+
+
