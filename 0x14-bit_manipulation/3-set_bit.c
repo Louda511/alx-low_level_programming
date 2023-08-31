9166a7e43da0e3af238b2a6a5d4669314ef5bafd
ahmed_khaled <companyofheroeseastern@gmail.com>
2023-08-31 21:07:43 +0200

    added 5-flip_bits.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
  * set_bit - set the bit at the index to one
  * @n: number
@@ -14,7 +13,6 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 31)
 		return (-1);
-
 	*n |= (1 << index);
 
 	return (1);
---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * flip_bits - returns the number of bits you flipped
+ * @n: first number
+ * @m: second number
+ *
+ * Return: returns the number of bits you flipped
+ *
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+{
+	int i, bit;
+	unsigned int count = 0;
+	unsigned long int xor;
+
+	xor = m ^ n;
+
+	for (i = 0; i < 31; i++)
+	{
+		bit = xor & (1 << i);
+		if (bit)
+			count++;
+
+	}
+	return (count);
+}
