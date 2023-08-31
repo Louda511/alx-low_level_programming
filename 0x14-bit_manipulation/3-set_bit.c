345c4fba93b03398ddb38eef3716f125e5e3014a
ahmed_khaled <companyofheroeseastern@gmail.com>
2023-08-31 20:43:58 +0200

    betty script for 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,5 +1,15 @@
 #include "main.h"
 
+
+/**
+ * set_bit - set the bit at the index to one
+ * @n: number
+ * @index: index
+ *
+ * Return: one if success or negative one if fail
+ *
+ */
+
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 31)
