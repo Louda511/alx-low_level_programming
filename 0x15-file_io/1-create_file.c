428c8f248b55ee979daf2c8c088051b44dc1d7cf
ahmed_khaled <companyofheroeseastern@gmail.com>
2023-09-07 19:44:33 +0300

    added 1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * create_file - creates a new file and write text to it
+ * @filename: name of the file to write to
+ * @text_content: text to be written to file
+ *
+ * Return: one on success negative one on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	FILE *fp;
+	size_t r;
+
+	if (filename == NULL)
+		return (-1);
+	
+	fp = fopen(filename, "w");
+
+	if (fp == NULL)
+		return(-1);
+	
+	if (text_content == NULL)
+		return(1);
+	r = fwrite(filename, 1, sizeof(filename), fp);
+
+	if (r)
+		return (1);
+	else
+		return (-1);
+}
+
+
+
